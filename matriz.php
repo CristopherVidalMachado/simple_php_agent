647475182f19c081faa88fa00647d70d78c2fd48
cristopher.vidal <cristopher@corp.kionux.com.br>
2020-02-19 17:08:42 -0300

    Adicionando componentes na dashboard

--- matriz.php
+++ matriz.php
@@ -11,68 +11,288 @@ $matriz = [];
 $matrizGrama = array_fill(0, tamanhoMatriz(), array_fill(0, tamanhoMatriz(), 1));
 
 
-function adicionaFormigueiros($matriz){
+function adicionaFormigueiros($matriz)
+{
     $arrayFormigueiros = array_fill(0, tamanhoMatriz(), array_fill(0, tamanhoMatriz(), 1));
     $counterFormigueiros = getTotalFormigueiros();
-    while($counterFormigueiros != 0){
-      
-        $linhaSelecionada = rand(0, tamanhoMatriz() -1);
-        $colunaSelecionada = rand(0, tamanhoMatriz() -1);
-        if($arrayFormigueiros[$linhaSelecionada][$colunaSelecionada] != 3){
-            $counterFormigueiros --;
+    while ($counterFormigueiros != 0) {
+        $linhaSelecionada = rand(0, tamanhoMatriz() - 1);
+        $colunaSelecionada = rand(0, tamanhoMatriz() - 1);
+        if ($arrayFormigueiros[$linhaSelecionada][$colunaSelecionada] != 3) {
+            $counterFormigueiros--;
         }
-        $arrayFormigueiros[$linhaSelecionada][$colunaSelecionada]=3;
+        $arrayFormigueiros[$linhaSelecionada][$colunaSelecionada] = 3;
     }
     return $arrayFormigueiros;
 }
 
-function mostarMatrizGramaComFormigueiros($array){
-    echo '<pre>';
-    for ($i=0; $i <tamanhoMatriz() ; $i++) {
-        for ($j=0; $j <tamanhoMatriz() ; $j++) {
+function mostarMatrizGramaComFormigueiros($array)
+{
+    for ($i = 0; $i < tamanhoMatriz(); $i++) {
+        for ($j = 0; $j < tamanhoMatriz(); $j++) {
             echo $array[$i][$j];
         }
         echo "\n";
     }
-    
-    echo '</pre>';
+
+    // echo '</pre>';
 }
 
 
 
 // print_r(getTotalFormigueiros());
 
-$array = adicionaFormigueiros($matrizGrama);
 
-mostarMatrizGramaComFormigueiros($array);
+function aaa($array)
+{
+    // echo '<pre>';
 
-echo "-------------";
-aaa($array);
 
-function aaa($array){
-    echo '<pre>';
-    
-    
-    for ($i=0; $i <tamanhoMatriz() ; $i++) {
-        
-     
-        for ($j =0; $j <tamanhoMatriz() ; $j++) {
-          $a =$j;
-            if($array[$i][$j] == 3){
+    for ($i = 0; $i < tamanhoMatriz(); $i++) {
+        for ($j = 0; $j < tamanhoMatriz(); $j++) {
+            $a = $j;
+            if ($array[$i][$j] == 3) {
                 echo "*";
                 continue;
             }
-            if($array[$i][$a++] == 3){
+            if ($array[$i][$a++] == 3) {
                 echo "next";
             }
 
-            
-            echo $array[$i][$j];
 
+            echo $array[$i][$j];
         }
 
         echo "\n";
     }
-    
-    echo '</pre>';
+
+    // echo '</pre>';
 }
+
+function irDireita($matriz, $coluna)
+{
+    //prox eh null
+    if (array_key_exists($coluna + 1, $matriz) == null) {
+        return -1;
+    }
+
+    //prox posso cortar
+    if ($matriz[$coluna + 1] == 1) {
+        return 1;
+    }
+
+    //prox ja cortei
+    if ($matriz[$coluna + 1] == 0) {
+        return 0;
+    }
+    //prox eh for formigueiro
+    if ($matriz[$coluna + 1] == 3) {
+        return -2;
+    }
+}
+
+function irEsquerda($matriz, $coluna)
+{
+    if (array_key_exists($coluna - 1, $matriz) == null) {
+        return -1;
+    }
+    if ($matriz[$coluna - 1] == 1) {
+        return 1;
+    }
+    if ($matriz[$coluna - 1] == 0) {
+        return 0;
+    }
+    if ($matriz[$coluna - 1] == 3) {
+        return -2;
+    }
+}
+
+function irBaixo($matriz, $linha, $coluna)
+{
+    if (array_key_exists($linha + 1, $matriz) == null || array_key_exists($coluna, $matriz[$linha + 1]) == null) {
+        return  -1;
+    }
+    if ($matriz[$linha + 1][$coluna] == 1) {
+        return 1;
+    }
+    if ($matriz[$linha + 1][$coluna] == 0) {
+        return 0;
+    }
+    if ($matriz[$linha + 1][$coluna] == 3) {
+        return -2;
+    }
+}
+
+function irCima($matriz, $linha, $coluna)
+{
+    if (array_key_exists($linha - 1, $matriz) == null || array_key_exists($coluna, $matriz[$linha - 1]) == null) {
+        return  -1;
+    }
+    if ($matriz[$linha - 1][$coluna] == 1) {
+        return 1;
+    }
+    if ($matriz[$linha - 1][$coluna] == 0) {
+        return 0;
+    }
+    if ($matriz[$linha - 1][$coluna] == 3) {
+        return -2;
+    }
+}
+
+function diagonalAltaDireita($matriz, $linha, $coluna)
+{
+    if (array_key_exists($linha - 1, $matriz) == null || array_key_exists($coluna + 1, $matriz[$linha - 1]) == null) {
+        return  -1;
+    }
+    if ($matriz[$linha - 1][$coluna + 1] == 1) {
+        return 1;
+    }
+    if ($matriz[$linha - 1][$coluna + 1] == 0) {
+        return 1;
+    }
+    if ($matriz[$linha - 1][$coluna + 1] == 3) {
+        return -2;
+    }
+}
+
+function diagonalBaixaDireita($matriz, $linha, $coluna)
+{
+    if (array_key_exists($linha + 1, $matriz) == null || array_key_exists($coluna + 1, $matriz[$linha + 1]) == null) {
+        return  -1;
+    }
+    if ($matriz[$linha + 1][$coluna + 1] == 1) {
+        return 1;
+    }
+    if ($matriz[$linha + 1][$coluna + 1] == 0) {
+        return 1;
+    }
+    if ($matriz[$linha + 1][$coluna + 1] == 3) {
+        return -2;
+    }
+}
+
+function diagonalAltaEsquerda($matriz, $linha, $coluna)
+{
+    if (array_key_exists($linha - 1, $matriz) == null || array_key_exists($coluna - 1, $matriz[$linha - 1]) == null) {
+        return  -1;
+    }
+
+    if ($matriz[$linha - 1][$coluna - 1] == 1) {
+        return 1;
+    }
+    if ($matriz[$linha - 1][$coluna - 1] == 0) {
+        return 1;
+    }
+    if ($matriz[$linha - 1][$coluna - 1] == 3) {
+        return -2;
+    }
+}
+
+function diagonalBaixaEsquerda($matriz, $linha, $coluna)
+{
+    if (array_key_exists($linha + 1, $matriz) == null || array_key_exists($coluna - 1, $matriz[$linha + 1]) == null) {
+        return  -1;
+    }
+
+    if ($matriz[$linha + 1][$coluna - 1] == 1) {
+        return 1;
+    }
+    if ($matriz[$linha + 1][$coluna - 1] == 0) {
+        return 1;
+    }
+    if ($matriz[$linha + 1][$coluna - 1] == 3) {
+        return -2;
+    }
+}
+
+
+echo "------------------\n";
+
+$array = adicionaFormigueiros($matrizGrama);
+// $array = $matrizGrama;
+mostarMatrizGramaComFormigueiros($array);
+
+echo "-------------\n";
+$array[0] = [1, 1,3,1];
+$array[1] = [1, 1,1,1];
+$array[2] = [1, 1,1,1];
+$array[3] = [1, 1,1,1];
+aaa($array);
+
+
+if ($array[0][0] == 1) {
+    $coluna = 0;
+    $linha = 0;
+
+    while (1 == 1) {
+        
+        $array[$linha][$coluna] = 0;
+
+        if (irDireita($array[$linha], $coluna) == 1) {
+            print_r("direita \n");
+            $coluna++;
+            continue;
+        }
+        elseif (DiagonalBaixaDireita($array, $linha, $coluna) == 1 ) {
+            print("Diagonal baixa direita\n");
+            $linha++;
+            $coluna++;
+            continue;
+        }
+
+        elseif (DiagonalAltaDireita($array, $linha, $coluna) == 1)  {
+            print("Diagonal alta direita\n");
+            if($linha > 0)
+                $linha--;
+           
+            $coluna++;
+            continue;
+        }
+        elseif (irBaixo($array, $linha, $coluna) == 1 )  {
+            print_r("baixo\n");
+            $linha++;
+            continue;
+        }
+
+        elseif (irEsquerda($array[$linha], $coluna) == 1) {
+            print_r("esquerda\n");
+            if($coluna >0)
+                $coluna--;
+            continue;
+        }
+        elseif (diagonalAltaEsquerda($array, $linha, $coluna) == 1) {
+            print("Diagonal alta esquerda\n");
+            if($linha >0)
+                $linha--;
+            if($coluna >0)
+                $coluna--;
+                continue;
+        }
+
+        elseif(diagonalBaixaEsquerda($array, $linha, $coluna) == 1 && irBaixo($array, $linha, $coluna) != 1){
+            print("Diagonal baixa esquerda\n");
+            $coluna--;
+            $linha++;
+            continue;
+
+        }
+
+        elseif (irCima($array, $linha, $coluna) == 1) {
+            print_r("cima\n");
+            if($coluna > 0)
+                $coluna--;
+            continue;
+        }
+        break;
+    }
+
+    echo "\n";
+
+    // }
+
+}
+
+mostarMatrizGramaComFormigueiros($array);
+
+
+
